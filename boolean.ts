9beb81b9ca4422be1134c5ea32523d09d6fec69e
mingdy98 <alswl1500@naver.com>
2022-03-30 00:15:03 +0900

    [feat] number, string, boolean 타입

---
+++ boolean.ts
@@ -0,0 +1,9 @@
+let isDone: boolean = false; //:boolean 표기를 해주는 것이 type annotaion임
+
+isDone = true;
+
+console.log(typeof isDone); // 'boolean'
+
+let isOk: Boolean = true;
+
+//let isNotOk: boolean = new Boolean(true);
---
+++ string.ts
@@ -0,0 +1,12 @@
+let myName: string = "Mark";
+
+//Template String
+
+let fullName: string = "Mark Lee";
+let age: number = 39;
+
+let sentence: string = `Hello, My name is ${fullName}.
+
+I'll be ${age + 1} years old next month.`;
+// => 띄어쓰기 , 엔터의 표현도 그대로 할 수 있다.
+console.log(sentence);
