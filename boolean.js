3fd110ef987fb14820d46e130c25f40996f15a59
mingdy98 <alswl1500@naver.com>
2022-03-31 11:10:22 +0900

    [feat] 작성자와 사용자의 관점으로 코드 바라보기

--- boolean.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let isDone = false; //:boolean 표기를 해주는 것이 type annotaion임
-isDone = true;
-console.log(typeof isDone); // 'boolean'
-let isOk = true;
-//let isNotOk: boolean = new Boolean(true);
--- number.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let decimal = 6; //10진수
-let hex = 0xf00d; //16진수
-let binary = 0b1010; //2진수
-let octal = 0o744; //8진수
-let notANumber = NaN; //가능.NaN도 숫자의 한 형태
-let underscoreNum = 1000000; //백만. 이렇게 사용하는것도 지원이됨.
